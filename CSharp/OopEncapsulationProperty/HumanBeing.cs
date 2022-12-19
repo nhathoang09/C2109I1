c56d52e5bd02ad93a8d846aceeb3b2b87a6a088e
Machine AptechLearning <machine.aptechlearning@outlook.com>
2022-12-19 13:25:27 +0700

    12/19/2022

---
+++ CSharp/OopEncapsulationProperty/HumanBeing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopEncapsulationProperty
+{
+    internal class HumanBeing
+    {
+        //fields các field phải là private
+        private string fullname;
+        //thuộc tính address và age theo C# mới
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        //property theo C# cũ (viết theo dạng mobile)
+        public string FullName{ set => fullname = value; get => fullname;}
+
+        public void ShowProper()
+        {
+            Console.WriteLine($"{nameof(fullname)} = {fullname}");
+            Console.WriteLine($"{nameof(Address)} = {Address}");
+        }
+    }
+}
---
+++ CSharp/OopEncapsulationProperty/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OopEncapsulationProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var human = new HumanBeing();
+            human.FullName = "Nhat Hoang";
+            human.Address = "123 abc";
+        }
+    }
+}
