25acf4b837e6e56dce49e73953856477d9e04284
Machine AptechLearning <machine.aptechlearning@outlook.com>
2022-12-19 14:32:46 +0700

    12/19/2022 2h32p

---
+++ CSharp/OopPolymorphismInterface/FileTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopPolymorphismInterface
+{
+    class FileTwo : IBinaryFile, ITextFile
+    {
+        //Không dùng bổ từ
+        void IBinaryFile.ReadFile() => Console.WriteLine("Read File");
+        void ITextFile.ReadFile() => Console.WriteLine("Read File");
+
+        //Bắt buộc dùng bổ từ
+        public void WriteBinaryFile() => Console.WriteLine("Write Binary File");
+
+        public void WriteTextFile() => Console.WriteLine("Write file");
+    }
+}
--- CSharp/OopPolymorphismInterface/Program.cs
+++ CSharp/OopPolymorphismInterface/Program.cs
@@ -12,6 +12,12 @@ namespace OopPolymorphismInterface
             f.ReadFile();
             ((IBinaryFile)f).ShowInfo();// Cũ
             (f as IBinaryFile).ShowInfo();// Mới
+
+            var ft = new FileOne();
+            ft.WriteBinaryFile();
+            ft.WriteTextFile();
+            (ft as IBinaryFile).ReadFile();
+            (ft as ITextFile).ReadFile();
         }
     }
 }
