4a794bd59604fe6b285ac3b4f4abb5e8530af98e
Machine AptechLearning <machine.aptechlearning@outlook.com>
2022-12-19 14:36:10 +0700

    2h36p

--- CSharp/OopPolymorphismInterface/Program.cs
+++ CSharp/OopPolymorphismInterface/Program.cs
@@ -6,18 +6,23 @@ namespace OopPolymorphismInterface
     {
         static void Main(string[] args)
         {
-            var f = new FileOne();
-            f.WriteBinaryFile();
-            f.WriteTextFile();
-            f.ReadFile();
-            ((IBinaryFile)f).ShowInfo();// Cũ
-            (f as IBinaryFile).ShowInfo();// Mới
+            //var f = new FileOne();
+            //f.WriteBinaryFile();
+            //f.WriteTextFile();
+            //f.ReadFile();
+            //((IBinaryFile)f).ShowInfo();// Cũ
+            //(f as IBinaryFile).ShowInfo();// Mới
 
-            var ft = new FileOne();
-            ft.WriteBinaryFile();
-            ft.WriteTextFile();
-            (ft as IBinaryFile).ReadFile();
-            (ft as ITextFile).ReadFile();
+            //var ft = new FileOne();
+            //ft.WriteBinaryFile();
+            //ft.WriteTextFile();
+            //(ft as IBinaryFile).ReadFile();
+            //(ft as ITextFile).ReadFile();
+
+            ITextFile itext = new FileOne();
+            itext.ReadFile();
+            itext = new FileTwo();
+            itext.ReadFile();
         }
     }
 }
