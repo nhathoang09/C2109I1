a795d5211d039862edeb9cc926b2d2f8da7f5619
Hoàng Nhật <nhathoangg09@gmail.com>
2022-12-26 14:15:12 +0700

    ac

---
+++ CSharp/ExeciseOop/Entity/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ExeciseOop.Entity
+{
+    internal class Product
+    {
+        public string Proid { get; set; }
+        public string ProName { get; set; }
+        public double ProPrice { get; set; }
+        public int ProQuantity { get; set; }
+        public DateTime ProDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Proid)}={Proid}, {nameof(ProName)}={ProName}, {nameof(ProPrice)}={ProPrice.ToString()}, {nameof(ProQuantity)}={ProQuantity.ToString()}, {nameof(ProDate)}={ProDate.ToString()}}}";
+        }
+    }
+}
