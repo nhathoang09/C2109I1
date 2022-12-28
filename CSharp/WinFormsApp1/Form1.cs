dd62cf4a62fc75cd97cf5168465db4af8e0e65ad
Nhật Hoàng <nhathoangg09@gmail.com>
2022-12-28 14:50:57 +0700

    Window Form

---
+++ CSharp/WinFormsApp1/Form1.cs
@@ -0,0 +1,23 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Hi", "Nhật Hoàng", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Yes", "Nhật Hoàng");
+            }
+            else
+            {
+                MessageBox.Show("No", "Nhật Hoàng");
+            }
+        }
+    }
+}
