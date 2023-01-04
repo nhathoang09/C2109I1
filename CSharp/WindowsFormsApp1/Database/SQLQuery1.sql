0dd05037c395dc81a912d82e1307b498fdb00c6e
Nhật Hoàng <nhathoangg09@gmail.com>
2023-01-04 13:04:02 +0700

    form

---
+++ CSharp/WindowsFormsApp1/Database/SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿USE master
+GO
+
+DROP DATABASE IF EXISTS C2109I1
+CREATE DATABASE C2109I1
+GO
+
+USE C2109I1
+GO
+
+DROP TABLE IF EXISTS Student
+CREATE TABLE Student
+(
+	Id int primary key identity,
+	FirstName nvarchar(50),
+	LastName nvarchar(50),
+	Gender tinyint,
+	DOB date
+)
+GO
+
+INSERT INTO Student(FirstName, LastName, Gender, DOB)
+VALUES (N'Nhật', N'Hoàng', 1, '2003-09-28')
+GO 10
